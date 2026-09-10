bd68aea264d0fe4951b37b622ed4bd27a5111c17
mayank-adobe <mayanksaxena@adobe.com>
2026-09-10 16:21:41 +0530

    Update project_data.py

--- month-01/python-basics/project_data.py
+++ month-01/python-basics/project_data.py
@@ -1,4 +1,4 @@
-# Week 2 Day 3
+# Week 2 Day 4
 
 project = {
     "project_name": "Workfront Planning Automation",
@@ -9,7 +9,10 @@ project = {
         "Requirement Gathering",
         "Project Creation",
         "Planning Record Update"
-    ]
+    ],
+    "risks": ["Missing requirements",
+              "Timeline delay"
+              ]
 }
 
 print("Project Details")
@@ -22,5 +25,24 @@ print(f"Active       : {project['active']}")
 
 print("\nTasks")
 
+if project["priority"] == "High":
+    print("Status : Urgent Project")
+
+elif project["priority"] == "Medium":
+    print("Status : Important Project")
+
+else:
+    print("Status : Normal Project")
+
 for task in project["tasks"]:
     print(f"- {task}")
+
+print("\nRisks")
+
+for risk in project["risks"]:
+    print(f"- {risk}")
+
+if len(project["tasks"]) > 2:
+    print("\nLarge Project")
+else:
+    print("Small Project")
