305aafa88d9fa7d697a30f159afefa7798641496
mayank-adobe <mayanksaxena@adobe.com>
2026-09-10 15:53:12 +0530

    Update project_data.py

--- month-01/python-basics/project_data.py
+++ month-01/python-basics/project_data.py
@@ -1,1 +1,26 @@
+# Week 2 Day 3
 
+project = {
+    "project_name": "Workfront Planning Automation",
+    "priority": "High",
+    "owner": "Mayank Saxena",
+    "active": True,
+    "tasks": [
+        "Requirement Gathering",
+        "Project Creation",
+        "Planning Record Update"
+    ]
+}
+
+print("Project Details")
+print("----------------")
+
+print(f"Project Name : {project['project_name']}")
+print(f"Priority     : {project['priority']}")
+print(f"Owner        : {project['owner']}")
+print(f"Active       : {project['active']}")
+
+print("\nTasks")
+
+for task in project["tasks"]:
+    print(f"- {task}")
